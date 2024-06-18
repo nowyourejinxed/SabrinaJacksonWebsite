cbb1633c333d808d43b7abb03cb522727da8b732
Sabrina <sabrinajackson@nevada.unr.edu>
2024-06-18 16:23:11 -0700

    convert to react/vite

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+
+      </div>
+      <h1>Sabrina Jackson</h1>
+      <div className="card">
+
+        <p>
+
+        </p>
+      </div>
+      <p className="read-the-docs">
+
+      </p>
+    </>
+  )
+}
+
+export default App
